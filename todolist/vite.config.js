8375d403d3d77a4b492beba4af183069dcdcb1dd
Bhargavi Krishnamurthi <bhargavikrish@mac.mynetworksettings.com>
2025-05-03 19:22:27 -0400

    changed vite config

--- todolist/vite.config.js
+++ todolist/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'Mern_todo',
+  base: '/Mern_todo/',
   build: {
     outDir: 'dist'
   },
